e490c25897f24cda388921c6b0498b2a33004738
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-07-29 10:34:48 -0500

    added tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,62 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      Product
+    ]
+  })
+  .then((dbTagData) => {
+    res.json(dbTagData)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    include: [
+      Product
+    ],
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((dbTagData) => {
+    res.json(dbTagData)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((dbTagData) => {
+    res.json(dbTagData)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((dbTagData) => {
+    res.json(dbTagData)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.delete({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((dbTagData) => {
+    res.json(dbTagData)
+  })
 });
 
 module.exports = router;
