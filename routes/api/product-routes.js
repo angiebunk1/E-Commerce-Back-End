a01b216be784dba6fe57c57ea99e8d5a7b5bae08
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-07-29 10:27:31 -0500

    finished category and product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,33 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      Category,
+      Tag
+    ]
+  })
+  .then((dbProductData) => {
+    res.json(dbProductData)
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    include: [
+      Category,
+      Tag
+    ],
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((dbProductData) => {
+    res.json(dbProductData)
+  })
 });
 
 // create new product
@@ -91,6 +112,14 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.delete({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((dbProductData) => {
+    res.json(dbProductData)
+  })
 });
 
 module.exports = router;
